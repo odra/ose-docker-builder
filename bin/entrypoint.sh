ecda9e44f82018b78ecdf46969d9ffaff60e36fa
Samuel Terburg <sterburg@redhat.com>
2016-11-28 15:35:50 +0000

    initial commit

---
+++ bin/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -x
+
+cat /etc/resolv.conf
+cat /etc/hostname
+cat /etc/hosts
+cat /run/secrets/kubernetes.io/serviceaccount/namespace
+
+export HTTP_PROXY=http://proxy.ops.svc.cluster.local:8080
+export HTTPS_PROXY=http://proxy.ops.svc.cluster.local:8080
+export http_proxy=http://proxy.ops.svc.cluster.local:8080
+export https_proxy=http://proxy.ops.svc.cluster.local:8080
+#export NO_PROXY
