385ff1eb6f81f994134d2d62e5b4dd10910d248e
Samuel Terburg <sterburg@redhat.com>
2016-11-29 10:25:48 +0000

    finish HTTP_PROXY variable

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,10 +3,17 @@
 set -x
 
 export OPS_NAMESPACE=`grep search /etc/resolv.conf |awk '{sub(".svc","-ops.svc", $2); print $2 }'`
-export HTTP_PROXY="http://proxy.$OPS_NAMESPACE:8080"
-export HTTPS_PROXY=$HTTP_PROXY
-export http_proxy=$HTTP_PROXY
-export https_proxy=$HTTP_PROXY
-#export NO_PROXY
+export PROXY_HOST="proxy.$OPS_NAMESPACE"
+export PROXY_PORT=8080
+
+## Only set proxy if proxy actually exists
+timeout 1 bash -c "cat < /dev/null > /dev/tcp/$PROXY_HOST/$PROXY_PORT"
+if [ "$?" == "0"]; then
+    export HTTP_PROXY="http://$PROXY_HOST:$PROXY_PORT"
+    export HTTPS_PROXY="$HTTP_PROXY"
+    export http_proxy="$HTTP_PROXY"
+    export https_proxy="$HTTP_PROXY"
+    #export NO_PROXY
+fi
 
 exec /usr/bin/openshift-docker-build "$@"
